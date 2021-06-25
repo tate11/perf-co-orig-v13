5971a2f9658ed227bc23cacd3306e65d86e314d2
captivea-cko <55201842+captivea-cko@users.noreply.github.com>
2021-06-25 10:07:43 -0400

    up
    up

---
+++ manufacture-reporting/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-manufacture-reporting",
+    description="Meta package for oca-manufacture-reporting Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-mrp_bom_current_stock',
+        'odoo13-addon-mrp_bom_matrix_report',
+        'odoo13-addon-mrp_bom_structure_report_level_1',
+        'odoo13-addon-mrp_bom_structure_xlsx',
+        'odoo13-addon-mrp_bom_structure_xlsx_level_1',
+        'odoo13-addon-mrp_flattened_bom_xlsx',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
